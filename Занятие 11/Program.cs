210ad36f3cb2357334d67257557898cb9230ec63
s.fedorovich <s.fedorovich@CZC71776GT>
2021-11-02 16:15:20 +0300

    Добавьте файлы проекта.

---
+++ Занятие 11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Занятие_11
+{
+    class Program
+    {//решение линейного уравнения
+        static void Main(string[] args)
+
+        
+            struct Reshenie
+        {
+            private double firstNumber;
+            private double secondNumber;
+            private double x;
+            public void
+
+        }
+            double k, b;
+            
+        }
+    }
+}
